57de4c0c98cab4db815283d014afc39e732239b9
Andrew.Golovkov <andrew.golovkov@gmail.com>
2009-09-14 17:58:33 +0400

    www-servers/nginx: new version: http://sysoev.ru/nginx/patch.180065.txt;

--- regen_manifest.sh
+++ regen_manifest.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 find . -iname '*.ebuild' | \
-while read f; do
-	if [[ $@ ]]; then
-		echo $f | egrep -v $@
-	else
-		echo $f
-	fi
-done | \
+{ [[ $@ ]] && grep $@ || cat; } | \
 xargs -I{} ebuild {} digest
