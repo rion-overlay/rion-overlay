b184193a6620191daf7025310eba20eb35ff93a4
Andrey Vinogradov <spamslepnoga@inbox.ru>
2010-06-08 02:22:59 +0300

    new  dev-python/python-krbV/

--- dev-python/python-krbV/files/setup.py
+++ dev-python/python-krbV/files/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup	(name = 'krbV',
-	version = '1.0.13',
+	version = '1.0.90',
 	description = 'Kerberos V Bindings for Python',
         long_description = """
 python-krbV allows python programs to use Kerberos 5 authentication/security
@@ -10,7 +10,7 @@ python-krbV allows python programs to use Kerberos 5 authentication/security
 	author_email = 'mikeb@redhat.com',
 	classifiers = [
 		'Development Status :: 4 - Beta',
-		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'License :: OSI Approved :: GNU General Public License (LGPL)',
 		'Operating System :: POSIX :: Linux',
 		'Programming Language :: C',
 		'Topic :: System :: Systems Administration :: Authentication/Directory'
