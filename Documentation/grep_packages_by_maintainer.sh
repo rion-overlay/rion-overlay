8d0bc163f49bae837c4e32276f65f96775d616f4
Ri0n <rion4ik@gmail.com>
2012-11-20 15:54:46 +0600

    oops. forgot to clean out myself from script

--- Documentation/grep_packages_by_maintainer.sh
+++ Documentation/grep_packages_by_maintainer.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ]; then
   exit 1;
 fi
 
-find ./ -name metadata.xml -exec grep '<email>rion4ik@gmail.com</email>' -H '{}' \; | sed 's:./\(.*\)/metadata.xml.*:\1:' | sort -u
+find ./ -name metadata.xml -exec grep "<email>$1</email>" -H '{}' \; | sed 's:./\(.*\)/metadata.xml.*:\1:' | sort -u
